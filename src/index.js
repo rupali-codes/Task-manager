e51b9dfb4be96f0661350351e421bcb26d15b1d4
rupali18082003 <missrupalithenaivegirl@gmail.com>
2022-02-20 20:43:36 +0530

    css bugs

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const { application } = require('express')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static(publicPath))
+app.use(express.static('publicPath'))
 app.use(userRouter)
 app.use(taskRouter)
 app.set('view engine', 'ejs')
