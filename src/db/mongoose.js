ed450b0390e3d7c3962a8893e24ee2cf1e11f32a
rupali-codes <rupali7487@gmail.com>
2022-09-20 20:00:51 +0530

    updating readme

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,6 +3,3 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URL, {
 	useNewUrlParser: true
 }).then(res => console.log("connected successfully")).catch(err => console.log("Error"))
-
-
-
